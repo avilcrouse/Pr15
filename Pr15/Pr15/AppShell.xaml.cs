3a098f3880ce05a7b2bc6cc52e35b33710bafe57
avilcrouse <kirilka.ru@bk.ru>
2022-05-22 16:40:10 +0300

    Добавьте файлы проекта.

---
+++ Pr15/Pr15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Pr15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Pr15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
